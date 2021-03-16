b8f7b4c59db8fddb779b909585087bc49e50ff0d
Yoshihisa AMAKATA <amakata@flatz.jp>
2021-03-16 21:08:19 +0900

    update vpc

--- lib/fargate-efs-stack.ts
+++ lib/fargate-efs-stack.ts
@@ -1,18 +1,31 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
+import * as ec2 from "@aws-cdk/aws-ec2";
 import * as cdk from '@aws-cdk/core';
 
 export class FargateEfsStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'FargateEfsQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const vpc = new ec2.Vpc(this, "FargateEfsVpc", {
+      cidr: "10.0.0.0/16",
+      enableDnsSupport: true,
+      enableDnsHostnames: true,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "Public",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: "Private",
+          subnetType: ec2.SubnetType.PRIVATE,
+        },
+        {
+          cidrMask: 24,
+          name: "Isolated",
+          subnetType: ec2.SubnetType.ISOLATED,
+        },
+      ],
     });
-
-    const topic = new sns.Topic(this, 'FargateEfsTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
